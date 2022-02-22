1151c81577bd9d1b2069c1fab20286e808cc4839
Toshi <leonardotkimura@gmail.com>
2022-02-22 12:17:22 -0300

    auto deploy keyguard

--- blockchain/services/connectService.js
+++ blockchain/services/connectService.js
@@ -6,6 +6,15 @@ const fs = require('fs')
 class ConnectService {
   constructor() {
     this.walletPath = path.join(process.cwd(), 'wallet');
+
+    if(process.env.CONTEXT=='microfabric'){
+      this.connectionProfilePath = path.resolve(__dirname, '..', 'fabric-details', 'microfabric-connection.json');
+      this.asLocalhost = true
+    }
+    else if(process.env.CONTEXT=='remote'){
+      this.connectionProfilePath = path.resolve(__dirname, '..', 'fabric-details', 'connection.json');
+      this.asLocalhost = false
+    }
     
   }
   
@@ -13,14 +22,11 @@ class ConnectService {
     const wallet = await Wallets.newFileSystemWallet(this.walletPath);
     console.log(`Wallet path: ${this.walletPath}`);
 
-    // const connectionProfilePath = path.resolve(__dirname, '..', 'fabric-details', 'remote-connection-larc.json');
-    const connectionProfilePath = path.resolve(__dirname, '..', 'fabric-details', 'connection.json');
-    // const connectionProfilePath = path.resolve(__dirname, '..', '..',  'blockchain', 'test-network', 'organizations', 'peerOrganizations', 'org1.example.com', 'connection-org1.json');
-    let connectionProfile = JSON.parse(fs.readFileSync(connectionProfilePath, 'utf8'));
+    let connectionProfile = JSON.parse(fs.readFileSync(this.connectionProfilePath, 'utf8'));
 
     const gateway = new Gateway();
     // let connectionOptions = { wallet, identity: 'userCertificate', discovery: { enabled: true, asLocalhost: true }};
-    let connectionOptions = { wallet, identity: 'keyguard', discovery: { enabled: true, asLocalhost: false }};
+    let connectionOptions = { wallet, identity: 'keyguard', discovery: { enabled: true, asLocalhost: this.asLocalhost }};
     await gateway.connect(connectionProfile, connectionOptions);
 
     const network = await gateway.getNetwork(channel);
--- wallet/parse-certificates.sh
+++ wallet/parse-certificates.sh
@@ -1,4 +1,3 @@
-node parser.js 'Org1 CA Admin.id' ca.crt certificate
 node parser.js 'keyguard.id' server.crt certificate
 node parser.js 'keyguard.id' server.key key
 node parser.js 'client.id' client.crt certificate
